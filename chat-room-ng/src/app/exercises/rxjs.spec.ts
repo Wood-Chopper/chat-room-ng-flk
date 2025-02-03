c2f2909b64fb59a56b5ebaaa3ccc0b767c42adfc
Jerome Navez <ext-jena@hms-networks.com>
2025-02-03 20:08:03 +0100

    more rxjs exercises

--- chat-room-ng/src/app/exercises/rxjs.spec.ts
+++ chat-room-ng/src/app/exercises/rxjs.spec.ts
@@ -75,5 +75,83 @@ describe('RxJs exercises', () => {
 
     const expected = [2, 4, 6, 8, 10];
     assert(result$, expected, done);
-  })
+  });
+
+  it('should map values to objects', (done) => {
+    const obs$ = of(1, 2, 3);
+
+    const result$ = obs$;//TODO line
+
+    const expected = [{value: 2}, {value: 4}, {value: 6}];
+    assert(result$, expected, done);
+  });
+
+  it('should catch and treat errors', (done) => {
+    const obs$ = of(5, 4, 3, 2, 1, 0);
+
+    const result$ = obs$;//TODO line
+
+    const expected = [10/5, 10/4, 10/3, 10/2, 10, Infinity];
+    assert(result$, expected, done);
+  });
+
+  it('should get the first value', (done) => {
+    const obs$ = of(5, 4, 3, 2, 1, 0);
+
+    const result$ = obs$;//TODO line
+
+    const expected = [5];
+    assert(result$, expected, done);
+  });
+
+  it('should map by returning an observable', (done) => {
+
+    function apiCall$(id: number): Observable<{id: number, value: string}> {
+      //Some API call returning an object
+      return of({id, value: id.toString()})
+    }
+
+    const obs$ = of(5, 4, 3);
+    //TODO subscription in subscription is a bad practice fix this code
+    const result$ = new Subject();
+    obs$.subscribe(id => {
+      apiCall$(id).subscribe(apiResult => {
+        result$.next(apiResult);
+      })
+    })
+
+    const expected = [
+      {id: 5, value: '5'},
+      {id: 4, value: '4'},
+      {id: 3, value: '3'},
+    ];
+    assert(result$, expected, done);
+  });
+
+
+  it('should generate an observable with 1000 values emitted', (done) => {
+    const result$ = of(0);//TODO line
+
+    const expected = [...Array(1000).keys()];
+    assert(result$, expected, done);
+  });
+
+  it('should start with a given value (0)', (done) => {
+    const obs$ = of(1, 2, 3);
+
+    const result$ = obs$;//TODO line
+
+    const expected = [0, 1, 2, 3];
+    assert(result$, expected, done);
+  });
+
+  it('should emit only when values change', (done) => {
+    const obs$ = of(1, 2, 2, 3, 4, 2, 2, 4, 5, 6, 6);
+
+    const result$ = obs$;//TODO line
+
+    const expected = [1, 2, 3, 4, 2, 4, 5, 6];
+    assert(result$, expected, done);
+  });
+
 });
