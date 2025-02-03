4d0befa155f5ff6326ab2d6456469ad607e43ec8
Jerome Navez <ext-jena@hms-networks.com>
2025-02-03 21:01:04 +0100

    more exercices

--- chat-room-ng/src/app/exercises/rxjs.spec.ts
+++ chat-room-ng/src/app/exercises/rxjs.spec.ts
@@ -1,118 +1,121 @@
-import { Observable, of, Subject } from 'rxjs';
-import DoneCallback = jest.DoneCallback;
+import { delay, Observable, of, Subject, tap } from 'rxjs';
+import { waitForAsync } from '@angular/core/testing';
 
 describe('RxJs exercises', () => {
-  function assertObs$(obs$: Observable<any>, expected: any[], done: DoneCallback) {
+  function assertObs$(obs$: Observable<any>, expected: any[]) {
     let index = 0;
 
     obs$.subscribe(
       {
-        next: (v) => expect(v).toEqual(expected[index++]),
+        next: (v) => {
+          expect(v).toEqual(expected[index++])
+        },
         error: e => fail(e),
         complete: () => {
-          expect(index).toBe(expected.length)
-          done()
+          expect(index).toEqual(expected.length)
         },
       },
     );
   }
 
-  it('should create an Observable', (done) => {
+  it('should create an Observable', waitForAsync(() => {
     // TODO modify the next line to create an Observable emitting 1, 2, 3
     const obs$: Observable<number> = null as unknown as Observable<number>;
 
     const expected = [1, 2, 3];
-    assertObs$(obs$, expected, done);
-  });
+    assertObs$(obs$, expected);
+  }));
 
-  it('should populate and complete an Observable', (done) => {
+  it('should populate and complete an Observable', waitForAsync(() => {
     const obs$: Subject<number> = new Subject();
 
     const expected = [1, 2, 3];
-    assertObs$(obs$, expected, done);
+    assertObs$(obs$, expected);
 
     //TODO here
-  });
 
-  it('should map values', (done) => {
+    obs$.complete()
+  }));
+
+  it('should map values', waitForAsync(() => {
     const obs$ = of(1, 2, 3);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [2, 4, 6];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should combine observables together', (done) => {
+  it('should combine observables together', waitForAsync(() => {
     const obs1$ = of(1, 2, 3);
     const obs2$ = of('a', 'b', 'c');
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = ['1a', '2b', '3c'];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should merge observables together', (done) => {
+  it('should merge observables together', waitForAsync(() => {
     const obs1$ = of(1, 2, 3);
     const obs2$ = of('a', 'b', 'c');
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [1, 'a', 2, 'b', 3, 'c'];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should limit the observable to 3 elements', (done) => {
+  it('should limit the observable to 3 elements', waitForAsync(() => {
     const obs$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [1, 2, 3];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should filter out odd values', (done) => {
+  it('should filter out odd values', waitForAsync(() => {
     const obs$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [2, 4, 6, 8, 10];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should map values to objects', (done) => {
+  it('should map values to objects', waitForAsync(() => {
     const obs$ = of(1, 2, 3);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [{value: 2}, {value: 4}, {value: 6}];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should catch and treat errors', (done) => {
+  it('should catch and treat errors', waitForAsync(() => {
     const obs$ = of(5, 4, 3, 2, 1, 0);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [10/5, 10/4, 10/3, 10/2, 10, Infinity];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should get the first value', (done) => {
+  it('should get the first value', waitForAsync(() => {
     const obs$ = of(5, 4, 3, 2, 1, 0);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [5];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should map by returning an observable', (done) => {
+  it('should map by returning an observable', waitForAsync(() => {
 
-    function apiCall$(id: number): Observable<{id: number, value: string}> {
+    function apiCall$(id: number): Observable<{id: number, response: string}> {
       //Some API call returning an object
-      return of({id, value: id.toString()})
+      return of({id, response: id.toString()}).pipe(delay(100))
     }
 
     const obs$ = of(5, 4, 3);
@@ -125,38 +128,72 @@ describe('RxJs exercises', () => {
     })
 
     const expected = [
-      {id: 5, value: '5'},
-      {id: 4, value: '4'},
-      {id: 3, value: '3'},
+      {id: 5, response: '5'},
+      {id: 4, response: '4'},
+      {id: 3, response: '3'},
     ];
-    assertObs$(result$, expected, done);
-    result$.complete();
-  });
+    assertObs$(result$, expected);
+    result$.complete();//TODO remove this
+  }));
 
 
-  it('should generate an observable with 1000 values emitted', (done) => {
+  it('should generate an observable with 1000 values emitted', waitForAsync(() => {
     const result$ = of(0);//TODO transform obs$
 
     const expected = [...Array(1000).keys()];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should start with a given value (0)', (done) => {
+  it('should start with a given value (0)', waitForAsync(() => {
     const obs$ = of(1, 2, 3);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [0, 1, 2, 3];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
 
-  it('should emit only when values change', (done) => {
+  it('should emit only when values change', waitForAsync(() => {
     const obs$ = of(1, 2, 2, 3, 4, 2, 2, 4, 5, 6, 6);
 
     const result$ = of(0);//TODO transform obs$
 
     const expected = [1, 2, 3, 4, 2, 4, 5, 6];
-    assertObs$(result$, expected, done);
-  });
+    assertObs$(result$, expected);
+  }));
+
+  it('should retry on API error', waitForAsync(() => {
+
+    function unreliableApiCall$(id: number): Observable<{id: number, response: string}> {
+      //Some API call returning an object
+      const response$ = of({id, response: id.toString()});
+      if (Math.random() < 0.5) {
+        // Simulate an issue
+        return response$.pipe(tap(() => {throw new Error()}))
+      }
+      return response$;
+    }
+
+    const obs$ = of(6, 5, 4, 3, 2, 1, 0);
+    //TODO subscription in subscription is a bad practice fix this code
+    const result$ = new Subject();
+    obs$.subscribe(id => {
+      unreliableApiCall$(id).subscribe(apiResult => {
+        result$.next(apiResult);
+      })
+    })
+
+    const expected = [
+      {id: 6, response: '6'},
+      {id: 5, response: '5'},
+      {id: 4, response: '4'},
+      {id: 3, response: '3'},
+      {id: 2, response: '2'},
+      {id: 1, response: '1'},
+      {id: 0, response: '0'},
+    ];
+    assertObs$(result$, expected);
+    result$.complete();//TODO remove this
+  }));
 
 });
